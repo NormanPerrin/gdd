5d9c2ba16635867c77901010631acfcdcf4c4d30
RubenMaier <RubenMaier@users.noreply.github.com>
2017-05-27 00:54:19 -0300

    Actualización del script SQL con las correciones
    Se corrigen todas las consideraciones presentadas por los representantes de la cátedra, pero aun no se han efectuado comparaciones de los cambios con el código C# por lo que puede haber inconsistencia

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -13,26 +13,46 @@ BEGIN
 	DROP PROCEDURE CRAZYDRIVER.spEditarIntentosUsuario;
 END;
 
+IF OBJECT_ID('CRAZYDRIVER.spObtenerRolesPorUsuario') IS NOT NULL
+BEGIN
+	DROP PROCEDURE CRAZYDRIVER.spObtenerRolesPorUsuario;
+END;
+
+IF OBJECT_ID('CRAZYDRIVER.spObtenerFuncionalidadesPorRol') IS NOT NULL
+BEGIN
+	DROP PROCEDURE CRAZYDRIVER.spObtenerFuncionalidadesPorRol;
+END;
+
 IF OBJECT_ID('CRAZYDRIVER.spObtenerRoles') IS NOT NULL
 BEGIN
 	DROP PROCEDURE CRAZYDRIVER.spObtenerRoles;
 END;
 
-IF OBJECT_ID('CRAZYDRIVER.spObtenerFuncionalidades') IS NOT NULL
-BEGIN
-	DROP PROCEDURE CRAZYDRIVER.spObtenerFuncionalidades;
-END;
-
 ---- BORRO TABLAS
 
-IF OBJECT_ID('CRAZYDRIVER.Viaje','U') IS NOT NULL
+IF OBJECT_ID('CRAZYDRIVER.FacturacionPorViaje','U') IS NOT NULL
 BEGIN
-   DROP TABLE CRAZYDRIVER.Viaje;
+   DROP TABLE CRAZYDRIVER.FacturacionPorViaje;
 END;
 
 IF OBJECT_ID('CRAZYDRIVER.Facturacion','U') IS NOT NULL
 BEGIN
    DROP TABLE CRAZYDRIVER.Facturacion;
+END;
+
+IF OBJECT_ID('CRAZYDRIVER.RendicionPorViaje','U') IS NOT NULL
+BEGIN
+   DROP TABLE CRAZYDRIVER.RendicionPorViaje;
+END;
+
+IF OBJECT_ID('CRAZYDRIVER.Rendicion','U') IS NOT NULL
+BEGIN
+   DROP TABLE CRAZYDRIVER.Rendicion;
+END;
+
+IF OBJECT_ID('CRAZYDRIVER.Viaje','U') IS NOT NULL
+BEGIN
+   DROP TABLE CRAZYDRIVER.Viaje;
 END;
 
 IF OBJECT_ID('CRAZYDRIVER.AutoPorChofer','U') IS NOT NULL
@@ -45,9 +65,14 @@ BEGIN
    DROP TABLE CRAZYDRIVER.Auto;
 END;
 
-IF OBJECT_ID('CRAZYDRIVER.Rendicion','U') IS NOT NULL
+IF OBJECT_ID('CRAZYDRIVER.Modelo','U') IS NOT NULL
 BEGIN
-   DROP TABLE CRAZYDRIVER.Rendicion;
+   DROP TABLE CRAZYDRIVER.Modelo;
+END;
+
+IF OBJECT_ID('CRAZYDRIVER.Marca','U') IS NOT NULL
+BEGIN
+   DROP TABLE CRAZYDRIVER.Marca;
 END;
 
 IF OBJECT_ID('CRAZYDRIVER.Turno','U') IS NOT NULL
@@ -60,14 +85,19 @@ BEGIN
    DROP TABLE CRAZYDRIVER.RolPorUsuario;
 END;
 
+IF OBJECT_ID('CRAZYDRIVER.Chofer','U') IS NOT NULL
+BEGIN
+   DROP TABLE CRAZYDRIVER.Chofer;
+END;
+
+IF OBJECT_ID('CRAZYDRIVER.Cliente','U') IS NOT NULL
+BEGIN
+   DROP TABLE CRAZYDRIVER.Cliente;
+END;
+
 IF OBJECT_ID('CRAZYDRIVER.Usuario','U') IS NOT NULL
 BEGIN
    DROP TABLE CRAZYDRIVER.Usuario;
-END;
-
-IF OBJECT_ID('CRAZYDRIVER.Persona','U') IS NOT NULL
-BEGIN
-   DROP TABLE CRAZYDRIVER.Persona;
 END;
 
 IF OBJECT_ID('CRAZYDRIVER.FuncionalidadPorRol','U') IS NOT NULL
@@ -117,28 +147,45 @@ CREATE TABLE CRAZYDRIVER.FuncionalidadPorRol(
 );
 GO
 
-CREATE TABLE CRAZYDRIVER.Persona(
-	id_persona INT IDENTITY PRIMARY KEY,
-	dni NUMERIC(18,0) UNIQUE NOT NULL,
-	apellido NVARCHAR(255) NOT NULL,
-	nombre NVARCHAR(255) NOT NULL,
-	telefono NUMERIC(18,0) NOT NULL,
-	mail NVARCHAR(255) NOT NULL,
-	fecha_nac DATETIME NOT NULL,
-	direccion NVARCHAR(255) NOT NULL,
-	nro_piso INT,
-	localidad NVARCHAR(255),
-	cod_postal INT
+CREATE TABLE CRAZYDRIVER.Usuario(
+	id_usuario INT IDENTITY PRIMARY KEY,
+	username NVARCHAR(225) NOT NULL ,
+	pass VARBINARY(225) NOT NULL,
+	habilitado TINYINT NOT NULL default 1,
+	intentos TINYINT NOT NULL default 0
 );
 GO
 
-CREATE TABLE CRAZYDRIVER.Usuario(
-	id_usuario INT IDENTITY PRIMARY KEY, -- no estoy seguro del not null porque el admin por ejemplo no va a tener una persona
-	username NVARCHAR(225),
-	pass VARBINARY(225),
-	id_persona INT FOREIGN KEY REFERENCES CRAZYDRIVER.Persona(id_persona),
-	habilitado TINYINT,
-	intentos TINYINT
+CREATE TABLE CRAZYDRIVER.Cliente(
+	id_cliente INT IDENTITY PRIMARY KEY,
+	dni NUMERIC(18,0) UNIQUE NOT NULL,
+	nombre NVARCHAR(255) NOT NULL,
+	apellido NVARCHAR(255) NOT NULL,
+	mail NVARCHAR(255) NOT NULL,
+	telefono NUMERIC(18,0) NOT NULL,
+	fecha_nac DATETIME NOT NULL,
+	direccion NVARCHAR(255) NOT NULL,
+	nro_piso INT, -- requisito nuevo
+	depto CHAR(1), -- requisito nuevo
+	localidad NVARCHAR(255), -- requisito nuevo
+	cod_postal INT, -- requisito nuevo
+	id_usuario INT NOT NULL FOREIGN KEY REFERENCES CRAZYDRIVER.Usuario(id_usuario)
+);
+GO
+
+CREATE TABLE CRAZYDRIVER.Chofer(
+	id_chofer INT IDENTITY PRIMARY KEY,
+	dni NUMERIC(18,0) UNIQUE NOT NULL,
+	nombre NVARCHAR(255) NOT NULL,
+	apellido NVARCHAR(255) NOT NULL,
+	mail NVARCHAR(255) NOT NULL,
+	telefono NUMERIC(18,0) NOT NULL,
+	fecha_nac DATETIME NOT NULL,
+	direccion NVARCHAR(255) NOT NULL,
+	nro_piso INT, -- requisito nuevo
+	depto INT, -- requisito nuevo
+	localidad NVARCHAR(255), -- requisito nuevo
+	id_usuario INT NOT NULL FOREIGN KEY REFERENCES CRAZYDRIVER.Usuario(id_usuario)
 );
 GO
 
@@ -157,22 +204,26 @@ CREATE TABLE CRAZYDRIVER.Turno(
 	valor_km NUMERIC(18,2),
 	precio_base NUMERIC(18,2) NOT NULL,
 	habilitado TINYINT
-	-- (hora_inicio, hora_fin) podria ser una primary key porque no voy a tener tuplas repetidas de estos datos en a tabla
 );
 GO
 
-CREATE TABLE CRAZYDRIVER.Rendicion(
-	nro_rendicion NUMERIC(18,0) PRIMARY KEY, --cambie id_rendicion por nro_rendicion ya que nro es unica en la maestra para este grupo de datos
-	fecha DATETIME NOT NULL,
-	importe NUMERIC(18,2)
-  );
+CREATE TABLE CRAZYDRIVER.marca(
+	id_marca INT IDENTITY PRIMARY KEY,
+	nombre NVARCHAR(255) Not NULL
+);
+GO
+
+CREATE TABLE CRAZYDRIVER.modelo(
+	id_modelo INT IDENTITY PRIMARY KEY,
+	nombre NVARCHAR(255) NOT NULL,
+	id_marca INT NOT NULL FOREIGN KEY REFERENCES CRAZYDRIVER.marca(id_marca),
+);
 GO
 
 CREATE TABLE CRAZYDRIVER.Auto(
 	id_auto INT IDENTITY PRIMARY KEY,
 	patente NVARCHAR(10) UNIQUE NOT NULL,
-	marca NVARCHAR(225) NOT NULL,
-	modelo NVARCHAR(225) NOT NULL,
+	id_modelo INT NOT NULL FOREIGN KEY REFERENCES CRAZYDRIVER.modelo(id_modelo),
 	licencia NVARCHAR(26) NOT NULL,
 	rodado NVARCHAR(10) NOT NULL,
 	habilitado TINYINT
@@ -181,33 +232,51 @@ GO
 
 CREATE TABLE CRAZYDRIVER.AutoPorChofer(
 	id_auto INT NOT NULL FOREIGN KEY REFERENCES CRAZYDRIVER.Auto(id_auto),
-	id_chofer INT NOT NULL FOREIGN KEY REFERENCES CRAZYDRIVER.Usuario(id_usuario),
+	id_chofer INT NOT NULL FOREIGN KEY REFERENCES CRAZYDRIVER.Chofer(id_chofer),
 	id_turno INT NOT NULL FOREIGN KEY REFERENCES CRAZYDRIVER.Turno(id_turno),
 	PRIMARY KEY (id_auto, id_chofer, id_turno)
-); --si no la vamos a usar para viaje y tenemos 3 keys en viaje es al pedo ya que cubrimos cualquier posibilidad en viaje de turno chofer y auto.
-GO
-
-CREATE TABLE CRAZYDRIVER.Facturacion(
-	nro_facturacion NUMERIC(18,0) PRIMARY KEY, --cambie id_facturacion por nro_facturacion ya que nro es unico para este grupo de datos
-	fecha_facturacion DATETIME NOT NULL,
-	fecha_inicio DATETIME NOT NULL,
-	fecha_fin DATETIME NOT NULL,
-	importe NUMERIC(18,2)
 );
 GO
 
 CREATE TABLE CRAZYDRIVER.Viaje(
 	id_viaje INT IDENTITY PRIMARY KEY,
-	id_cliente INT NOT NULL FOREIGN KEY REFERENCES CRAZYDRIVER.Usuario(id_usuario),
-	id_chofer INT NOT NULL FOREIGN KEY REFERENCES CRAZYDRIVER.Usuario(id_usuario),
+	id_cliente INT NOT NULL FOREIGN KEY REFERENCES CRAZYDRIVER.Cliente(id_cliente),
+	id_chofer INT NOT NULL FOREIGN KEY REFERENCES CRAZYDRIVER.Chofer(id_chofer),
 	id_turno INT NOT NULL FOREIGN KEY REFERENCES CRAZYDRIVER.Turno(id_turno),
-	id_auto INT NOT NULL FOREIGN KEY REFERENCES CRAZYDRIVER.Auto(id_auto), -- en realidad no hace falta, pero para hacer el estadistico nos haria pegarle menos a la db al querer obtener el dato
+	id_auto INT NOT NULL FOREIGN KEY REFERENCES CRAZYDRIVER.Auto(id_auto),
 	fecha DATETIME NOT NULL,
-	cant_km NUMERIC(18,0) NOT NULL,
-	nro_facturacion NUMERIC(18,0) FOREIGN KEY REFERENCES CRAZYDRIVER.Facturacion(nro_facturacion),
-	nro_rendicion NUMERIC(18,0) FOREIGN KEY REFERENCES CRAZYDRIVER.Rendicion(nro_rendicion)
-	-- PRIMARY KEY (id_cliente, id_chofer, id_turno, id_auto, fecha) podria ser pero los boludos del tp no pusieron fechas exactas en la db
+	cant_km NUMERIC(18,0) NOT NULL
 );
+GO
+
+CREATE TABLE CRAZYDRIVER.Rendicion(
+	nro_rendicion NUMERIC(18,0) PRIMARY KEY,
+	fecha DATETIME NOT NULL
+  );
+GO
+
+CREATE TABLE CRAZYDRIVER.RendicionPorViaje(
+	nro_rendicion NUMERIC(18,0) NOT NULL FOREIGN KEY REFERENCES CRAZYDRIVER.Rendicion(nro_rendicion),
+	id_viaje INT NOT NULL FOREIGN KEY REFERENCES CRAZYDRIVER.Viaje(id_viaje),
+	importe NUMERIC(18,2),
+	PRIMARY KEY (nro_rendicion, id_viaje)
+  );
+GO
+
+CREATE TABLE CRAZYDRIVER.Facturacion(
+	nro_facturacion NUMERIC(18,0) PRIMARY KEY, --cambie id_facturacion por nro_facturacion ya que nro es unico para este grupo de datos
+	fecha DATETIME NOT NULL,
+	fecha_inicio DATETIME NOT NULL,
+	fecha_fin DATETIME NOT NULL
+);
+GO
+
+CREATE TABLE CRAZYDRIVER.FacturacionPorViaje(
+	nro_facturacion NUMERIC(18,0) NOT NULL FOREIGN KEY REFERENCES CRAZYDRIVER.Facturacion(nro_facturacion),
+	id_viaje INT NOT NULL FOREIGN KEY REFERENCES CRAZYDRIVER.Viaje(id_viaje),
+	importe NUMERIC(18,2),
+	PRIMARY KEY (nro_facturacion, id_viaje)
+  );
 GO
 
 ---- ROLES Y FUNCIONALIDES
@@ -256,9 +325,9 @@ GO
 
 DECLARE @hash_password VARBINARY(225)
 	SELECT @hash_password = HASHBYTES('SHA2_256', N'w23e');
-INSERT INTO CRAZYDRIVER.Usuario(username, pass, id_persona, habilitado, intentos)
+INSERT INTO CRAZYDRIVER.Usuario(username, pass, habilitado, intentos)
 	values
-		('admin', HASHBYTES('SHA2_256', N'w23e'), null, 1, 0)
+		('admin', HASHBYTES('SHA2_256', N'w23e'), 1, 0)
 GO
 
 INSERT INTO CRAZYDRIVER.RolPorUsuario(id_usuario, id_rol)
@@ -268,43 +337,50 @@ GO
 
 ---- MIGRACION
 
-INSERT INTO CRAZYDRIVER.Persona(dni, apellido, nombre, telefono, mail, fecha_nac, direccion, nro_piso, localidad, cod_postal)
-	SELECT
-		m.Chofer_Dni, m.Chofer_Apellido, m.Chofer_Nombre, m.Chofer_Telefono, m.Chofer_Mail, m.Chofer_Fecha_Nac, m.Chofer_Direccion, null, null, null
-	FROM
-		gd_esquema.Maestra m
-    UNION
-	SELECT
-		m.Cliente_Dni, m.Cliente_Apellido, m.Cliente_Nombre, m.Cliente_Telefono, m.Cliente_Mail, m.Cliente_Fecha_Nac, m.Cliente_Direccion, null, null, null
-	FROM
-		gd_esquema.Maestra m
-GO
-
 DECLARE @hash_password VARBINARY(225)
 	SELECT @hash_password = HASHBYTES('SHA2_256', N'w23e');
-INSERT INTO CRAZYDRIVER.Usuario(username, pass, id_persona, habilitado, intentos)
-	SELECT 
-		(SELECT CAST(p.dni AS NVARCHAR(225))), @hash_password, p.id_persona, 1, 0
+INSERT INTO CRAZYDRIVER.Usuario(username, pass, habilitado, intentos)
+	SELECT
+		(SELECT CAST(Chofer_Dni AS NVARCHAR(225))), HASHBYTES('SHA2_256', N'w23e'), 1, 0
 	FROM
-		CRAZYDRIVER.Persona p
+		gd_esquema.Maestra
+	UNION SELECT 
+		(SELECT CAST(Cliente_Dni AS NVARCHAR(225))), HASHBYTES('SHA2_256', N'w23e'), 1, 0
+	FROM
+		gd_esquema.Maestra
+GO
+
+INSERT INTO CRAZYDRIVER.Chofer(dni, apellido, nombre, telefono, mail, fecha_nac, direccion, nro_piso, depto, localidad, id_usuario)
+	SELECT DISTINCT
+		m.Chofer_Dni, m.Chofer_Apellido, m.Chofer_Nombre, m.Chofer_Telefono, m.Chofer_Mail, m.Chofer_Fecha_Nac, m.Chofer_Direccion, null, null, null, u.id_usuario
+	FROM
+		gd_esquema.Maestra m, CRAZYDRIVER.Usuario u
+	WHERE
+		cast(m.Chofer_Dni as varchar(255)) = u.username
+GO
+
+INSERT INTO CRAZYDRIVER.Cliente(dni, apellido, nombre, telefono, mail, fecha_nac, direccion, nro_piso, depto, localidad, cod_postal, id_usuario)
+	SELECT DISTINCT
+		m.Cliente_Dni, m.Cliente_Apellido, m.Cliente_Nombre, m.Cliente_Telefono, m.Cliente_Mail, m.Cliente_Fecha_Nac, m.Cliente_Direccion, null, null, null, null, u.id_usuario
+	FROM
+		gd_esquema.Maestra m, CRAZYDRIVER.Usuario u
+	WHERE
+		cast(m.Cliente_Dni as varchar(255)) = u.username
 GO
 
 INSERT INTO CRAZYDRIVER.RolPorUsuario(id_rol, id_usuario) -- cargo usuarios con rol de clientes
-	SELECT DISTINCT
-		2, u.id_usuario
+	SELECT
+		2, cl.id_usuario
 	FROM
-		gd_esquema.Maestra m, CRAZYDRIVER.Usuario u, CRAZYDRIVER.Persona p
+		gd_esquema.Maestra m,  CRAZYDRIVER.Cliente cl
 	WHERE
-		m.Cliente_Dni = p.dni AND p.id_persona = u.id_persona
-GO
-
-INSERT INTO CRAZYDRIVER.RolPorUsuario(id_rol, id_usuario) -- cargo usuarios con rol de choferes
-	SELECT DISTINCT
-		3, u.id_usuario
+		m.Cliente_Dni = cl.dni
+	UNION SELECT
+		3, ch.id_usuario
 	FROM
-		gd_esquema.Maestra m, CRAZYDRIVER.Usuario u, CRAZYDRIVER.Persona p
+		gd_esquema.Maestra m,  CRAZYDRIVER.Chofer ch
 	WHERE
-        m.Chofer_Dni = p.dni AND p.id_persona = u.id_persona
+		m.Chofer_Dni = ch.dni	
 GO
 
 INSERT INTO CRAZYDRIVER.Turno(hora_inicio, hora_fin, descripcion, valor_km, precio_base, habilitado)
@@ -314,65 +390,128 @@ INSERT INTO CRAZYDRIVER.Turno(hora_inicio, hora_fin, descripcion, valor_km, prec
 		gd_esquema.Maestra m
 GO
 
-INSERT INTO CRAZYDRIVER.Auto(patente, marca, modelo, licencia, rodado, habilitado)
+INSERT INTO CRAZYDRIVER.Marca(nombre)
 	SELECT DISTINCT
-		m.Auto_Patente, m.Auto_Marca, m.Auto_Modelo, m.Auto_Licencia, m.Auto_Rodado, 1
+		Auto_Marca
+	FROM
+		gd_esquema.Maestra
+GO
+
+INSERT INTO CRAZYDRIVER.Modelo(nombre, id_marca)
+	SELECT DISTINCT
+		Auto_Modelo, ma.id_marca
+	FROM
+		gd_esquema.Maestra
+		JOIN CRAZYDRIVER.Marca ma
+			ON ma.nombre = Auto_Marca
+GO
+
+INSERT INTO CRAZYDRIVER.Auto(patente, id_modelo, licencia, rodado, habilitado)
+	SELECT DISTINCT
+		m.Auto_Patente, mo.id_modelo, m.Auto_Licencia, m.Auto_Rodado, 1
 	FROM
 		gd_esquema.Maestra m
-	GO
+		JOIN CRAZYDRIVER.Modelo mo
+			ON mo.nombre = m.Auto_Modelo
+		JOIN CRAZYDRIVER.Marca ma
+			ON ma.nombre = m.Auto_Marca
+GO
 
 INSERT INTO CRAZYDRIVER.AutoPorChofer(id_auto, id_chofer, id_turno)
 	SELECT DISTINCT
-		a.id_auto, u.id_usuario, t.id_turno
+		a.id_auto, ch.id_chofer, t.id_turno
 	FROM
-		gd_esquema.Maestra m, CRAZYDRIVER.Turno t, CRAZYDRIVER.Usuario u, CRAZYDRIVER.Persona p, CRAZYDRIVER.Auto a
+		gd_esquema.Maestra m, CRAZYDRIVER.Turno t, CRAZYDRIVER.Chofer ch, CRAZYDRIVER.Auto a
 	WHERE
 		m.Auto_Patente = a.patente
-		AND (m.Chofer_Dni = p.dni AND p.id_persona = u.id_persona)
+		AND (m.Chofer_Dni = ch.dni)
 		AND (m.Turno_Hora_Inicio = t.hora_inicio AND m.Turno_Hora_Fin = t.hora_fin)
 GO
 
-INSERT INTO CRAZYDRIVER.Facturacion(nro_facturacion, fecha_facturacion, fecha_inicio, fecha_fin,importe)
+INSERT INTO CRAZYDRIVER.Viaje(id_cliente, id_chofer, id_turno, id_auto, fecha, cant_km)
 	SELECT 
-		Factura_Nro, Factura_Fecha, Factura_Fecha_Fin, Factura_Fecha_Inicio, CAST( ROUND((SUM(Viaje_Cant_Kilometros * Turno_Valor_Kilometro + Turno_Precio_Base)),2) AS NUMERIC(18,2))
-    FROM 
-		gd_esquema.Maestra 
-	WHERE 
-		Factura_Nro IS NOT NULL
-		group by Factura_Nro, Factura_Fecha, Factura_Fecha_Fin, Factura_Fecha_Inicio
+		cl.id_cliente, ch.id_chofer, t.id_turno, a.id_auto, m.Viaje_Fecha, m.Viaje_Cant_Kilometros
+	FROM 
+		gd_esquema.Maestra m
+			JOIN CRAZYDRIVER.Cliente cl
+				ON m.Cliente_Dni = cl.dni
+			JOIN CRAZYDRIVER.Chofer ch 
+				ON m.Chofer_Dni = ch.dni
+			JOIN CRAZYDRIVER.Turno t
+				ON m.Turno_Hora_Fin = t.hora_fin
+				AND m.Turno_Hora_Inicio = t.hora_inicio
+			JOIN CRAZYDRIVER.Auto a
+				ON m.Auto_Patente = a.patente
+	WHERE
+		m.Rendicion_Nro IS NULL AND m.Factura_Nro IS NULL
 GO
 
-INSERT INTO CRAZYDRIVER.Rendicion(nro_rendicion, fecha,importe)
-	SELECT 
-		Rendicion_Nro, Rendicion_Fecha, CAST( ROUND( SUM(Rendicion_Importe), 2) as NUMERIC(18,2))
+INSERT INTO CRAZYDRIVER.Rendicion(nro_rendicion, fecha)
+	SELECT DISTINCT
+		Rendicion_Nro, Rendicion_Fecha
 	FROM 
 		gd_esquema.Maestra
 	WHERE 
 		Rendicion_Nro IS NOT NULL
-		group by Rendicion_Nro, Rendicion_Fecha
 GO
 
-INSERT INTO CRAZYDRIVER.Viaje(id_cliente, id_chofer, id_turno, id_auto, fecha, cant_km, nro_facturacion, nro_rendicion)
-	SELECT DISTINCT 
-		ucl.id_usuario, uch.id_usuario, t.id_turno, a.id_auto, m.Viaje_Fecha, m.Viaje_Cant_Kilometros, MAX(Factura_Nro), MAX(Rendicion_Nro)
-	FROM 
-		gd_esquema.Maestra m 
-			JOIN CRAZYDRIVER.Persona cl
-				ON m.Cliente_Dni = cl.dni
-			JOIN CRAZYDRIVER.Persona ch 
-				ON m.Chofer_Dni = ch.dni
-			JOIN CRAZYDRIVER.Turno t  
-				ON m.Turno_Descripcion = t.descripcion 
-				AND m.Turno_Hora_Fin = t.hora_fin
+INSERT INTO CRAZYDRIVER.RendicionPorViaje(nro_rendicion, id_viaje, importe) 
+	SELECT DISTINCT
+		m.Rendicion_Nro, v.id_viaje, m.Rendicion_Importe
+	FROM
+		gd_esquema.Maestra m
+			JOIN CRAZYDRIVER.Viaje v
+				ON v.cant_km = m.Viaje_Cant_Kilometros
+				AND v.fecha = m.Viaje_Fecha
+			JOIN CRAZYDRIVER.Turno t
+				ON m.Turno_Hora_Fin = t.hora_fin
 				AND m.Turno_Hora_Inicio = t.hora_inicio
-				AND m.Turno_Precio_Base = t.precio_base
-				AND m.Turno_Valor_Kilometro = t.valor_km
+				AND v.id_turno = t.id_turno
+			JOIN CRAZYDRIVER.Cliente cl
+				ON m.Cliente_Dni = cl.dni
+				AND v.id_cliente = cl.id_cliente
+			JOIN CRAZYDRIVER.Chofer ch 
+				ON m.Chofer_Dni = ch.dni
+				AND v.id_chofer = ch.id_chofer
 			JOIN CRAZYDRIVER.Auto a
 				ON m.Auto_Patente = a.patente
-			JOIN CRAZYDRIVER.Usuario uch ON uch.id_persona = ch.id_persona
-			JOIN CRAZYDRIVER.Usuario ucl ON ucl.id_persona = cl.id_persona
-	GROUP BY 
-		ucl.id_usuario, uch.id_usuario, t.id_turno, a.id_auto, m.Viaje_Fecha, m.Viaje_Cant_Kilometros;
+				AND v.id_auto = a.id_auto
+	WHERE
+		m.Rendicion_Nro IS NOT NULL 
+GO
+
+INSERT INTO CRAZYDRIVER.Facturacion(nro_facturacion, fecha, fecha_inicio, fecha_fin)
+	SELECT DISTINCT
+		Factura_Nro, Factura_Fecha, Factura_Fecha_Inicio, Factura_Fecha_Fin
+	FROM 
+		gd_esquema.Maestra
+	WHERE 
+		Factura_Nro IS NOT NULL
+GO
+
+INSERT INTO CRAZYDRIVER.FacturacionPorViaje(nro_facturacion, id_viaje, importe)
+    SELECT DISTINCT
+		m.Factura_Nro, v.id_viaje, (m.Viaje_Cant_Kilometros * m.Turno_Valor_Kilometro + m.Turno_Precio_Base)
+	FROM
+		gd_esquema.Maestra m
+			JOIN CRAZYDRIVER.Viaje v
+				ON v.cant_km = m.Viaje_Cant_Kilometros
+				AND v.fecha = m.Viaje_Fecha
+			JOIN CRAZYDRIVER.Turno t
+				ON m.Turno_Hora_Fin = t.hora_fin
+				AND m.Turno_Hora_Inicio = t.hora_inicio
+				AND v.id_turno = t.id_turno
+			JOIN CRAZYDRIVER.Cliente cl
+				ON m.Cliente_Dni = cl.dni
+				AND v.id_cliente = cl.id_cliente
+			JOIN CRAZYDRIVER.Chofer ch 
+				ON m.Chofer_Dni = ch.dni
+				AND v.id_chofer = ch.id_chofer
+			JOIN CRAZYDRIVER.Auto a
+				ON m.Auto_Patente = a.patente
+				AND v.id_auto = a.id_auto
+	WHERE
+		m.Factura_Nro IS NOT NULL 
 GO
 
 ---- PROCEDIMIENTOS ALMACENADOS
@@ -380,7 +519,7 @@ GO
 CREATE PROC CRAZYDRIVER.spLogin
 	@username NVARCHAR(255)
 	AS
-	SELECT id_usuario, pass, ISNULL(id_persona, -1), intentos
+	SELECT id_usuario, pass, intentos
 		FROM CRAZYDRIVER.Usuario
 		WHERE username=@username
 GO
@@ -393,7 +532,7 @@ CREATE PROC CRAZYDRIVER.spEditarIntentosUsuario
 		WHERE id_usuario=@idUsuario
 GO
 
-CREATE PROC CRAZYDRIVER.spObtenerRoles
+CREATE PROC CRAZYDRIVER.spObtenerRolesPorUsuario
 	@idUsuario INT
 	AS
 		SELECT DISTINCT r.nombre 
@@ -401,11 +540,17 @@ CREATE PROC CRAZYDRIVER.spObtenerRoles
 		WHERE rpu.id_usuario = @idUsuario and rpu.id_rol = r.id_rol and r.habilitado = 1
 GO
 
-CREATE PROC CRAZYDRIVER.spObtenerFuncionalidades
+CREATE PROC CRAZYDRIVER.spObtenerFuncionalidadesPorRol
 	@nombreRol NVARCHAR(100)
 	AS
 		SELECT DISTINCT f.descripcion
 		FROM CRAZYDRIVER.FuncionalidadPorRol fpr, CRAZYDRIVER.Rol r, CRAZYDRIVER.Funcionalidad f
 		WHERE r.nombre = @nombreRol and r.id_rol = fpr.id_rol and fpr.habilitado = 1
 		and fpr.id_funcionalidad = f.id_funcionalidad
+GO
+
+CREATE PROC CRAZYDRIVER.spObtenerRoles
+	AS
+		SELECT DISTINCT nombre 
+		FROM CRAZYDRIVER.Rol
 GO
