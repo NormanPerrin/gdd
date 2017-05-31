91b965598f951a10634a568c787c604c51f3dda5
santiagoMaresco <santiagomaresco@gmail.com>
2017-05-31 04:58:07 -0300

    Subo la capa de negocio para auto

---
+++ src/CapaNegocio/NAuto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class NAuto
+    {
+
+
+        public static string alta(string patente, string marca, string modelo, string licencia, string rodado)
+        {
+            CapaDatos.DAuto objeto = new CapaDatos.DAuto();
+            return objeto.AgregarAuto(patente, marca, modelo, licencia, rodado);
+        }
+
+    }
+}
