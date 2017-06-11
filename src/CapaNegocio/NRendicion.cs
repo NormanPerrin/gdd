b50a06973ce2e6dcbd115ea9f3ed51d05855df71
santiagoMaresco <santiagomaresco@gmail.com>
2017-06-11 16:10:02 -0300

    subo NRendicion

---
+++ src/CapaNegocio/NRendicion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class NRendicion
+    {
+        public static object ObtenerViajes(DateTime fecha, int turno, int idChofer)
+        {
+            CapaDatos.DRendicion Objeto = new CapaDatos.DRendicion();
+            return Objeto.ObtenerViajes(fecha, turno, idChofer);
+        }
+    }
+}
