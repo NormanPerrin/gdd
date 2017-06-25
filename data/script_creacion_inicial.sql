94272d2f438a0c74e538e243e1595fceb4544b3f
santiagoMaresco <santiagomaresco@gmail.com>
2017-06-24 21:49:01 -0300

    agrego sp necesario para rendicion de viajes

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -273,6 +273,11 @@ BEGIN
 	DROP PROCEDURE CRAZYDRIVER.spEliminarAuto;
 END;
 
+IF OBJECT_ID('CRAZYDRIVER.spObtenerViajes') IS NOT NULL
+BEGIN
+	DROP PROCEDURE CRAZYDRIVER.spObtenerViajes;
+END;
+
 ---- BORRO TABLAS
 
 IF OBJECT_ID('CRAZYDRIVER.FacturacionPorViaje','U') IS NOT NULL
@@ -1599,4 +1604,19 @@ CREATE PROC CRAZYDRIVER.spEliminarAuto
 	@idAuto int
 	AS
 	UPDATE CRAZYDRIVER.Auto set habilitado = 0 where id_auto = @idAuto;
+GO
+
+CREATE PROC CRAZYDRIVER.spObtenerViajes
+	@fecha datetime,
+	@turno int,
+	@chofer int
+	AS
+		declare @fechaSinHora datetime = CONVERT (char(10), @fecha, 103);
+
+		SELECT c.id_chofer, v.id_turno, v.fecha_inicio, (SELECT (t.precio_base+v.cant_km * t.valor_km)
+			FROM CRAZYDRIVER.Turno t
+			WHERE t.id_turno = v.id_turno) as importe
+		FROM CRAZYDRIVER.Chofer c
+		JOIN CRAZYDRIVER.Viaje v on c.id_chofer = v.id_chofer
+		WHERE c.id_chofer = @chofer AND (CONVERT (char(10),v.fecha_inicio, 103)) = @fechaSinHora AND v.id_turno = @turno
 GO
